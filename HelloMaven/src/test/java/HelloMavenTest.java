6354d592feff79ebd3ff6524004c707cab1f51be
Taffazzel Hossain <tafaz_hossain@yahoo.com>
2017-08-08 16:59:24 +0100

    one test added

--- HelloMaven/src/test/java/HelloMavenTest.java
+++ HelloMaven/src/test/java/HelloMavenTest.java
@@ -8,7 +8,7 @@ public class HelloMavenTest {
     public void getName1() throws Exception {
         HelloMaven hm = new HelloMaven("HelloMaven");
 	HelloMaven hm2 = new HelloMaven("Hello");
-	HelloMaven hm3 = new HelloMaven("World")
+	HelloMaven hm3 = new HelloMaven("World");
         assertEquals("HelloMaven", hm.getName());
 	assertEquals("Hello", hm2.getName());
 	assertEquals("World",hm3.getName());
