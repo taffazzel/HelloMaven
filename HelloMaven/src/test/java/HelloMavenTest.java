d3f955cb9013a6741af23b5d985ae134e8a74c94
Taffazzel Hossain <tafaz_hossain@yahoo.com>
2017-08-08 15:52:35 +0100

    One error removed

--- HelloMaven/src/test/java/HelloMavenTest.java
+++ HelloMaven/src/test/java/HelloMavenTest.java
@@ -7,7 +7,7 @@ public class HelloMavenTest {
     @org.junit.Test
     public void getName1() throws Exception {
         HelloMaven hm = new HelloMaven("HelloMaven");
-	HelloMaven hm2 = new HelloMaven("Hello")
+	HelloMaven hm2 = new HelloMaven("Hello");
         assertEquals("HelloMaven", hm.getName());
 	assertEquals("Hello", hm2.getName());
 
